c0d90ad3556d3fbb262ac6d11d13279526305128
wiilwaal <suldanwilwal@gmail.com>
2021-08-26 01:23:27 +0300

    add new changes

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,22 @@
 package dao;
 
-public class NewsDao {
+import models.News;
+
+import java.util.List;
+
+public interface NewsDao {
+    //Add new for a department
+    void add(News news);
+
+    //Find news by the id
+    News findById(int id);
+
+    //Update department news
+    void updateNews(int id, String title, String content,int departmentId);
+
+    //Get all department news by department id
+    List< News > getAllNews();
+
+    void clearAll();
 }
+
